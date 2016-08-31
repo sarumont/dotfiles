3b052390c81063cf656f4195103024acfa048b01
Richard Kolkovich <richard@sigil.org>
2016-08-31 13:31:03 -0500

    don't check upstream if none exists

--- sh/omz-custom/mygit.zsh
+++ sh/omz-custom/mygit.zsh
@@ -1,6 +1,6 @@
 # Gets the number of commits ahead from remote
 function git_remote_prompt() {
-  if $(command git rev-parse --git-dir > /dev/null 2>&1); then
+  if $(command git rev-parse --abbrev-ref --symbolic-full-name @{u} > /dev/null 2>&1); then
     local COMMITS="$(git rev-list --count @{upstream}..HEAD)"
     if [[ $COMMITS -ne 0 ]]; then
         echo -n "$ZSH_THEME_GIT_COMMITS_AHEAD_PREFIX$COMMITS$ZSH_THEME_GIT_COMMITS_AHEAD_SUFFIX"
