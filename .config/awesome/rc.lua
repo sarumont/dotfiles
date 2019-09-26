9c86390306e1632bb43777f29586fcab64c79b22
Richard Kolkovich <richard@sigil.org>
2019-09-26 06:46:27 -0600

    force searching in HOME

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -62,7 +62,7 @@ awful.spawn.with_shell(
     'if (xrdb -query | grep -q "^awesome\\.started:\\s*true$"); then exit; fi;' ..
     'xrdb -merge <<< "awesome.started:true";' ..
     -- list each of your autostart commands, followed by ; inside single quotes, followed by ..
-    'dex --environment Awesome --autostart --search-paths "$XDG_CONFIG_HOME/autostart"' -- https://github.com/jceb/dex
+    'dex --environment Awesome --autostart --search-paths "' .. os.getenv("HOME") .. '/.config/autostart"' -- https://github.com/jceb/dex
 )
 
 -- }}}
