8b4de57011007d784e7b856b480c0913623d5778
Richard Kolkovich <richard@sigil.org>
2020-05-28 08:49:28 -0600

    update Java aliases

--- .aliases.zsh
+++ .aliases.zsh
@@ -61,8 +61,8 @@ alias gup='git up' # defer this to ~/gitconfig
 alias st='scm_st'
 
 # JDK switching
-alias jdk8="sdk use java 8.0.222-zulu"
-alias jdk11="sdk use java 19.3.0.r11-grl"
+alias jdk8="sdk use java 8.0.252-zulu"
+alias jdk11="sdk use java 20.1.0.r11-grl"
 
 alias certbot='certbot --config-dir ~/.config/letsencrypt --logs-dir ~/tmp --work-dir ~/tmp'
 
