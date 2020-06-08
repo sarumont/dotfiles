60b99a893546296ac0012b74eafed5288eb2592d
Richard Kolkovich <richard@sigil.org>
2020-06-08 08:54:21 -0600

    Allow width to be configured by user variables

--- .config/awesome/widgets/rounded_bar.lua
+++ .config/awesome/widgets/rounded_bar.lua
@@ -1,6 +1,7 @@
 local gears = require("gears")
 local wibox = require("wibox")
 local beautiful = require("beautiful")
+local user = require('user')
 
 return function(active_color, background_color, icon)
     local bar = wibox.widget{
@@ -11,7 +12,7 @@ return function(active_color, background_color, icon)
             top = dpi(8),
             bottom = dpi(8),
         },
-        forced_width  = dpi(100),
+        forced_width  = dpi(user.gauge_width or 100),
         shape         = gears.shape.rounded_bar,
         bar_shape     = gears.shape.rounded_bar,
         color         = active_color,
