346acbcb0ecb5c23d7bfd0152e862ad12cfe1025
Richard Kolkovich <richard@sigil.org>
2021-01-20 20:56:09 -0700

    make full_pull more efficient (thanks, @notlesh)

--- .aliases.zsh
+++ .aliases.zsh
@@ -50,7 +50,7 @@ alias update_submodules='git pull --recurse-submodules && git submodule update -
 alias _git_full_log="git log --graph --oneline --decorate"
 alias _git_prunable='git branch --merged | grep -v "\*" | egrep -v "(master|develop)"'
 alias bump='git commit -m "⬆️"'
-alias full_pull='git pull --rebase && git fetch --all --prune && git branch -d `git branch --merged | grep -v "\*" | egrep -v "(master|develop|richard)"`'
+alias full_pull='git pull --all --prune --rebase && git branch -d `git branch --merged | grep -v "\*" | egrep -v "(master|develop|richard)"`'
 
 # OMZ overrides
 alias gcm='git commit -m' 
