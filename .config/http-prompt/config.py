473dd58174d223ea6bdc0d76bcc5f7c9d48966a6
sarumont <richard@sigil.org>
2021-07-01 10:18:12 -0600

    add http-prompt config

--- .config/http-prompt/config.py
+++ .config/http-prompt/config.py
@@ -3,12 +3,12 @@
 # fruity, igor, lovelace, manni, monokai, murphy, native, paraiso-dark,
 # paraiso-light, pastie, perldoc, rrt, solarized, tango, trac, vim, vs, xcode.
 # Preview themes at http://http-prompt.com/themes
-command_style = 'monokai'
+command_style = 'autumn'
 
 # Highlighting style for HTTPie's output. Available values are the same as
 # command_style. Set this to None to use HTTPie's default style, which you
 # can refer to https://httpie.org/doc#config-file-location
-output_style = None
+output_style = 'autumn'
 
 # The tool used to paginate output. Available values: 'less' and 'more'.
 # Note that 'more' does not support ANSI colors.
