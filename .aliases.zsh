39b667968485db44373f5283ed87c423485b12cb
sarumont <richard@sigil.org>
2021-11-15 12:10:09 -0700

    update pomodoro aliases to use playerctl

--- .aliases.zsh
+++ .aliases.zsh
@@ -79,9 +79,9 @@ alias beet="$(which beet) --config ~/.local/beets/config.yaml"
 alias mymounts="tree -L 1 /media/$USER"
 
 # productivity
-alias pomodoro="mpc play && at now + 25 minutes <<< 'mpc pause && notify-send -i ~/Dropbox/tomato.png -u normal \"Take a break\"'"
-alias super_pom="mpc play && at now + 50 minutes <<< 'mpc pause && notify-send -i ~/Dropbox/tomato.png -u normal \"Take a break\"'"
-alias deep_work="mpc play && at now + 90 minutes <<< 'mpc pause && notify-send -i ~/Dropbox/tomato.png -u normal \"Take a break\"'"
+alias pomodoro="playerctl play && at now + 25 minutes <<< 'playerctl pause && notify-send -i ~/Dropbox/tomato.png -u normal \"Take a break and do some burpees\"'"
+alias super_pom="playerctl play && at now + 50 minutes <<< 'playerctl pause && notify-send -i ~/Dropbox/tomato.png -u normal \"Take a break and do some burpees\"'"
+alias deep_work="playerctl play && at now + 90 minutes <<< 'playerctl pause && notify-send -i ~/Dropbox/tomato.png -u normal \"Take a break and do some burpees\"'"
 
 if [[ -r ~/.local/sh/aliases.zsh ]]; then
     . ~/.local/sh/aliases.zsh
