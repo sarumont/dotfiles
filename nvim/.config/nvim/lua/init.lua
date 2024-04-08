491b1cb42ab90b978b53e5650a5c03d5ae02a179
Richard Kolkovich <richard@moov.io>
2024-04-08 15:31:04 -0600

    move old init to options

--- nvim/.config/nvim/lua/init.lua
+++
@@ -1,7 +0,0 @@
-
-vim.cmd([[au BufReadPost * if expand('%:p') !~# '\m/\.git/' && line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif]])
-
--- automatically rebalance windows on vim resize
-vim.cmd([[au VimResized * :wincmd =]])
-
-vim.fn.sign_define('DapBreakpoint', {text='🔴', texthl='', linehl='', numhl=''})
--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -4,3 +4,10 @@ local o = vim.o
 
 o.cursorlineopt = 'both' -- to enable cursorline!
 o.laststatus = 2
+
+vim.fn.sign_define('DapBreakpoint', {text='🔴', texthl='', linehl='', numhl=''})
+
+-- automatically rebalance windows on vim resize
+vim.cmd([[au VimResized * :wincmd =]])
+
+vim.cmd([[au BufReadPost * if expand('%:p') !~# '\m/\.git/' && line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif]])
