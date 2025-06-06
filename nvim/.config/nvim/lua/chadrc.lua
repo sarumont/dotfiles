67ef73e875a456d9a59afb27332b825a23848788
Richard Kolkovich <richard@moov.io>
2025-06-06 10:28:28 -0600

    formatting

--- nvim/.config/nvim/lua/chadrc.lua
+++ nvim/.config/nvim/lua/chadrc.lua
@@ -1,25 +1,25 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "onenord",
+  theme = "onenord",
   theme_toggle = { "onenord", "onenord_light" },
 
-	hl_override = {
-		Comment = { italic = true },
-		["@comment"] = { italic = true },
-	},
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+  },
 }
 
 M.nvdash = { load_on_startup = false }
 M.ui = {
-      tabufline = {
-         enabled = false
-     }
+  tabufline = {
+    enabled = false,
+  },
 }
 
 return M
