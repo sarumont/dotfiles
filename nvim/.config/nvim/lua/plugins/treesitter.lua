76eac9bce6bf7e79eab7c412180530b0b78ba94b
Richard Kolkovich <richard@moov.io>
2025-10-09 16:51:16 -0600

    chore: update to main treesitter branch

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,7 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    branch = "main",
     config = function()
       require("nvim-treesitter.configs").setup {
         ensure_installed = {
