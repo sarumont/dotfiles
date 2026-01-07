3cb07a47bb714d6a42848d9e9e5458bcf07a0ad1
Richard Kolkovich <richard@moov.io>
2026-01-07 12:08:34 -0700

    nvim tweaks, stash

--- nvim/.config/nvim/lua/plugins/neotest.lua
+++ nvim/.config/nvim/lua/plugins/neotest.lua
@@ -5,39 +5,62 @@ return {
     dependencies = {
       "nvim-neotest/nvim-nio",
       "nvim-lua/plenary.nvim",
-      "nvim-neotest/neotest-go",
-      "nvim-treesitter/nvim-treesitter",
+      -- "nvim-neotest/neotest-go",
+      "fredrikaverpil/neotest-golang",
     },
-    opts = {
-      adapters = {
-        ["neotest-go"] = {
-          recursive_run = true,
-        },
-      },
-    },
-  },
-  {
-    "nvim-neotest/neotest-go",
-    ft = "go",
-    dependencies = "nvim-neotest/neotest",
     config = function()
-      -- get neotest namespace (api call creates or returns namespace)
-      local neotest_ns = vim.api.nvim_create_namespace "neotest"
-      vim.diagnostic.config({
-        virtual_text = {
-          format = function(diagnostic)
-            local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
-            return message
-          end,
-        },
-      }, neotest_ns)
       require("neotest").setup {
+        options = {
+          warn_test_name_dupes = false,
+        },
         adapters = {
-          require "neotest-go",
+          require "neotest-golang" {
+            -- Here we can set options for neotest-golang, e.g.
+            -- go_test_args = { "-v", "-race", "-count=1", "-timeout=60s" },
+            dap_go_enabled = true, -- requires leoluz/nvim-dap-go
+            runner = "gotestsum",
+            go_test_args = {
+              "-v",
+              "-race",
+              "-count=1",
+              "-cover",
+              -- "-coverprofile=" .. vim.fn.getcwd() .. "/coverage.out",
+            },
+          },
         },
       }
     end,
   },
+  {
+    "fredrikaverpil/neotest-golang",
+    version = "*", -- Optional, but recommended; track releases
+    build = function()
+      vim.system({ "go", "install", "gotest.tools/gotestsum@latest" }):wait() -- Optional, but recommended
+    end,
+  },
+
+  -- {
+  --   "nvim-neotest/neotest-go",
+  --   ft = "go",
+  --   dependencies = "nvim-neotest/neotest",
+  --   config = function()
+  --     -- get neotest namespace (api call creates or returns namespace)
+  --     local neotest_ns = vim.api.nvim_create_namespace "neotest"
+  --     vim.diagnostic.config({
+  --       virtual_text = {
+  --         format = function(diagnostic)
+  --           local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+  --           return message
+  --         end,
+  --       },
+  --     }, neotest_ns)
+  --     require("neotest").setup {
+  --       adapters = {
+  --         require "neotest-go",
+  --       },
+  --     }
+  --   end,
+  -- },
   {
     "leoluz/nvim-dap-go",
     ft = "go",
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    branch = "main",
+    branch = "master",
     config = function()
       require("nvim-treesitter.configs").setup {
         ensure_installed = {
